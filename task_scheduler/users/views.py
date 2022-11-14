3160cceeec5dc9a849ff317d75c0025ba493d0e1
Reza Rajabi <29972744+RezaRjbi@users.noreply.github.com>
2022-11-14 16:49:10 +0330

    add future tasks that need to be done

--- task_scheduler/users/views.py
+++ task_scheduler/users/views.py
@@ -72,3 +72,32 @@ class LogoutView(APIView):
     def post(self, request, format=None):
         request.user.auth_token.delete()
         return response(data="Successfully logged out", status_code=status.HTTP_200_OK)
+
+
+class PromoteToAdminView(APIView):
+    """
+    a superuser can promote a normal user to a staff member
+    """
+
+    def post(self):
+        # todo
+        ...
+
+
+class DemoteUserView(APIView):
+    """
+    a superuser can demote a staff user to a normal user
+    """
+
+    def post(self):
+        # todo
+        ...
+
+
+class ChangeUserActivateStatusView(APIView):
+    """
+    turn a user active status to de-active and vice-versa. only staff members can perform this action
+    """
+    def post(self):
+        # todo
+        ...
