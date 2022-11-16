4d6985f4bb9cbc11f078076f7fbf3c49d9924f73
Reza Rajabi <29972744+RezaRjbi@users.noreply.github.com>
2022-11-16 13:16:52 +0330

    add superuser permission

--- task_scheduler/users/permissions.py
+++ task_scheduler/users/permissions.py
@@ -29,3 +29,7 @@ class IsAdminOrOwner(permissions.BasePermission):
                 request.user and request.user.is_staff
             )
 
+
+class IsSuperuser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
