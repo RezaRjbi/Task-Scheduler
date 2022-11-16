293c2be9d8a13fc26298f823a29d3da0d90464f4
Reza Rajabi <29972744+RezaRjbi@users.noreply.github.com>
2022-11-16 13:10:49 +0330

    use new permissions, fixed login

--- task_scheduler/users/views.py
+++ task_scheduler/users/views.py
@@ -1,20 +1,21 @@
 from rest_framework.views import APIView, status
 from rest_framework.authtoken.models import Token
 
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import get_object_or_404
+from django.db.utils import IntegrityError
 
 from .models import User
-from .serializers import UserSerializer, LoginSerializer, UpdateUserSerializer
+from .serializers import UserSerializer, RegisterUserSerializer, LoginSerializer, UpdateUserSerializer
 
-from .permissions import IsAdminOrNewUser
+from .permissions import IsAdminOrNewUser, IsAdminOrOwner
 
 from utils.general import response
 from utils.db import update_instance
 
 
 class ListCreateUserView(APIView):
-    # permission_classes = [IsAdminOrNewUser]
+    permission_classes = [IsAdminOrNewUser]
 
     def get(self, request):
         users = User.objects.all()
@@ -23,14 +24,22 @@ class ListCreateUserView(APIView):
         )
 
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = RegisterUserSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
+            try:
+
+                user = User.objects.create_user(**serializer.data)
+            except IntegrityError as e:
+                return response(
+                    status_code=status.HTTP_400_BAD_REQUEST, errors=str(e), detail="username already exists"
+                )
             return response(instance=user, status_code=status.HTTP_201_CREATED, serializer=UserSerializer)
         return response(errors=serializer.errors, status_code=status.HTTP_400_BAD_REQUEST)
 
 
 class RetrieveUpdateDestroyUserView(APIView):
+    permission_classes = [IsAdminOrOwner]
+
     def get(self, request, pk: int):
         user = get_object_or_404(User, pk=pk)
         return response(instance=user, serializer=UserSerializer, status_code=status.HTTP_200_OK)
@@ -59,16 +68,16 @@ class LoginView(APIView):
             return response(errors=serializer.errors, status_code=status.HTTP_400_BAD_REQUEST)
         data = serializer.data
         try:
-            User.objects.get(username=data["username"])
+            u = User.objects.get(username=data.get("username"))
         except User.DoesNotExist as e:
             return response(detail="user not found", errors=str(e), status_code=status.HTTP_404_NOT_FOUND)
-        user = authenticate(username=data["username"], password=data["password"])
+        user = authenticate(username=data.get("username"), password=data.get("password"))
         if not user:
             return response(
                 detail="provided username and password dont match",
                 errors="wrong_credentials", status_code=status.HTTP_401_UNAUTHORIZED
             )
-
+        login(request, user)
         token = Token.objects.get_or_create(user=user)
         return response(
             detail=f"{user.username} logged in successfully",
@@ -79,6 +88,7 @@ class LoginView(APIView):
 class LogoutView(APIView):
 
     def post(self, request, format=None):
+        logout(request)
         request.user.auth_token.delete()
         return response(detail="Successfully logged out", status_code=status.HTTP_200_OK)
 
@@ -97,6 +107,7 @@ class ChangeUserActivateStatusView(APIView):
     """
     turn a user active status to de-active and vice-versa. only staff members can perform this action
     """
+
     def post(self):
         # todo
         ...
