6560edc72bced5fca8fd2a8d825d89fc03f38f8c
Reza Rajabi <29972744+RezaRjbi@users.noreply.github.com>
2022-11-16 13:11:49 +0330

    new tests & add auth for tests

--- task_scheduler/users/test/test_user_management.py
+++ task_scheduler/users/test/test_user_management.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
+
 from rest_framework.test import APIClient
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -13,11 +16,19 @@ class UserManagementTestCase(TestCase):
     def setUpTestData(cls):
         cls.user_url = reverse("users")
 
-    def test_user_register(self):
-        response = self.client.post(self.user_url, data={"username": "testUser", "password": "password"})
-        user = User.objects.get(username="testUser")
-        serializer = UserSerializer(user)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        User.objects.create(username="testUser", password="password")
+        User.objects.create(username="testUser2", password="password")
+        User.objects.create(username="testUser3", password="password")
+        cls.admin_user = User.objects.create(username="testAdmin", password="password", is_staff=True)
+        cls.superuser = User.objects.create_superuser(username="superUser", password="password")
+
+    def setUp(self) -> None:
+        self.admin_user_token = Token.objects.get_or_create(user=self.admin_user)[0].key
+        self.superuser_token = Token.objects.get_or_create(user=self.superuser)[0].key
+
+    def test_get_user_list(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_user_token)
+        response = self.client.get(self.user_url)
+        serializer = UserSerializer(User.objects.all(), many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()["instances"], serializer.data)
-
-
--- task_scheduler/users/test/test_users.py
+++ task_scheduler/users/test/test_users.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 from rest_framework.test import APIClient
 from rest_framework import status
-
+from rest_framework.authtoken.models import Token
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -14,29 +14,57 @@ class UserTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user_url = reverse("users")
+        cls.admin_user = User.objects.create(username="testAdmin", password="password", is_staff=True)
 
     def setUp(self):
-        self.sample_user = User.objects.create(username="testUser", password="password")
-        self.sample_user.refresh_from_db()
+        self.sample_user = User.objects.create_user(username="testUser", password="password")
+        self.sample_user_token = Token.objects.get_or_create(user=self.sample_user)[0].key
+        self.admin_user_token = Token.objects.get_or_create(user=self.admin_user)[0].key
+
+    def test_user_register(self):
+        response = self.client.post(self.user_url, data={"username": "testUser2", "password": "password"})
+        user = User.objects.get(username="testUser2")
+        serializer = UserSerializer(user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()["instances"], serializer.data)
 
     def test_identical_username_registration(self):
         response = self.client.post(self.user_url, data={"username": "testUser", "password": "password2"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_retrieve_user(self):
-        response = self.client.get(f"{self.user_url}1/")
+    def test_retrieve_user_by_admin(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_user_token)
+        id = self.sample_user.id
+        response = self.client.get(f"{self.user_url}{id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         serializer = UserSerializer(self.sample_user)
         self.assertEqual(response.json()["instances"], serializer.data)
 
-    def test_update_user(self):
-        response = self.client.put(f"{self.user_url}1/", {"phone": "09355556589", "email": "a@aiij.com"})
+    def test_retrieve_user_by_owner(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.sample_user_token)
+        id = self.sample_user.id
+        response = self.client.get(f"{self.user_url}{id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        updated_user = UserSerializer(User.objects.get(pk=1))
+        serializer = UserSerializer(self.sample_user)
+        self.assertEqual(response.json()["instances"], serializer.data)
+
+    def test_retrieve_user_by_others(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.sample_user_token)
+        response = self.client.get(f"{self.user_url}3/")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_user(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.sample_user_token)
+        id = self.sample_user.id
+        response = self.client.put(f"{self.user_url}{id}/", {"first_name": "firstname", "email": "a@aiij.com"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_user = UserSerializer(User.objects.get(pk=id))
         self.assertEqual(response.json()["instances"], updated_user.data)
 
     def test_delete_user(self):
-        response = self.client.delete(f"{self.user_url}1/")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_user_token)
+        id = self.sample_user.id
+        response = self.client.delete(f"{self.user_url}{id}/")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         with self.assertRaisesMessage(User.DoesNotExist, "User matching query does not exist."):
-            User.objects.get(pk=1)
+            User.objects.get(username=self.sample_user.username)
