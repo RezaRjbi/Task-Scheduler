77c800a9cfbb2808e80a7567dcca601b21641f6c
Reza Rajabi <29972744+RezaRjbi@users.noreply.github.com>
2022-11-17 13:09:12 +0330

    add object level permission

--- task_scheduler/users/permissions.py
+++ task_scheduler/users/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 
 class IsAdminOrNewUser(permissions.BasePermission):
@@ -12,24 +13,52 @@ class IsAdminOrNewUser(permissions.BasePermission):
         return True
 
 
-class IsAdminOrOwner(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        requested_id = int(request.path.split("/")[-2])
-        if request.method == "GET":
-            return bool(
-                request.user and (request.user.is_staff or request.user.id == requested_id)
-            )
-        if request.method == "PUT":
-            return bool(
-                request.user and request.user.id == requested_id
-            )
-        if request.method == "DELETE":
-            return bool(
-                request.user and request.user.is_staff
-            )
-
-
 class IsSuperuser(permissions.BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_superuser)
+
+
+class CustomPermissions:
+
+    @classmethod
+    def get_req_and_kwarg(cls, args: tuple, kwargs: dict, kw: str, convert_to_int: bool = True):
+        """
+        get the request object from pos-arguments and kw from keyword-arguments from decorated functions.
+        we need this values to set different permissions for different users
+        """
+        value = kwargs.get(kw)
+        if convert_to_int:
+            int(value)
+        return args[1], value
+
+    @classmethod
+    def is_admin_or_owner(cls, f):
+        def wrapper(*args, **kwargs):
+            request, pk = cls.get_req_and_kwarg(args, kwargs, "pk")
+            if bool(request.user and (request.user.id == pk or request.user.is_staff)):
+                return f(*args, **kwargs)
+            raise PermissionDenied
+
+        return wrapper
+
+    @classmethod
+    def is_owner(cls, f):
+        def wrapper(*args, **kwargs):
+            request, pk = cls.get_req_and_kwarg(args, kwargs, "pk")
+
+            if bool(request.user and request.user.id == pk):
+                return f(*args, **kwargs)
+            raise PermissionDenied
+
+        return wrapper
+
+    @classmethod
+    def is_admin(cls, f):
+        def wrapper(*args, **kwargs):
+            request, pk = cls.get_req_and_kwarg(args, kwargs, "pk")
+
+            if bool(request.user and request.user.is_staff):
+                return f(*args, **kwargs)
+            raise PermissionDenied
+
+        return wrapper
